5f5b4ad1db13d63064dbcf787bf0e4736e365bf9
Orion Grieco <46628700+Orion-Grieco@users.noreply.github.com>
2024-08-26 21:03:22 -0400

    updated barcode generation
    updated the tool to automatically keep track of the amount of created barcodes and base file name off of said metric.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import barcode
+import os.path
 
 from barcode.writer import ImageWriter
 
 number = input("Enter barcode number: ")
 writer = ImageWriter()
 barcode = barcode.get_barcode_class("code128")
-barcode(number, writer=writer).save("barcode.png")
+barcode(number, writer=writer).save(f"images/barcode{sum(os.path.isfile(os.path.join("images", f)) for f in os.listdir("images"))}")
